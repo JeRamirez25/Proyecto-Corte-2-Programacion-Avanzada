8b542ffc53c7a42dfe646750dbae1ceb38ee7fa9
Juan E Ramírez <juanesramirezgalan@gmail.com>
2024-03-29 14:24:07 -0500

    Inicio de 0 por multiples errores en el codigo original

--- main.py
+++ main.py
@@ -1,198 +1,163 @@
 import time
+#----------------------------------------------------------ZAPATOS--------------------------------------------------------------------------
+class Zapatos:
+    producto = ''
+    precio = 0
+    talla = ''
 
-class IProducto:
-    def seleccionar(self):
+class Adidas(Zapatos):
+    producto = 'Adidas'
+    precio = 299
+    talla = '9'
+
+class NewBalance(Zapatos):
+    producto = 'New Balance'
+    precio = 249
+    talla = '10'
+
+class Nike(Zapatos):
+    producto = 'Nike'
+    precio = 199
+    talla = '8'
+
+#-----------------------------------------------------------CELULARES--------------------------------------------------------------------------
+class Celulares:
+    producto = ''
+    precio = 0
+    almacenamiento = ''
+    procesador = ''
+    sistema_operativo = ''
+    memoria_ram = ''
+
+class iPhone11(Celulares):
+    producto = 'iPhone 11'
+    precio = 2549
+    almacenamiento = '128GB'
+    procesador = 'A13 Bionic Chip'
+    sistema_operativo = 'IOS'
+    memoria_ram = '4GB'
+
+
+class iPhone13(Celulares):
+    producto = 'iPhone 13'
+    precio = 3089
+    almacenamiento = '128GB'
+    procesador = 'A15 Bionic Chip'
+    sistema_operativo = 'IOS'
+    memoria_ram = '4GB'
+
+class GalaxyS23(Celulares):
+    producto = 'Samsung Galaxy S23 Ultra'
+    precio = 5499
+    almacenamiento = '256GB'
+    procesador = 'Samsung Qualcomm SM8550'
+    sistema_operativo = 'Android'
+    memoria_ram = '12GB'
+
+#------------------------------------------------------------INVENTARIO--------------------------------------------------------------------------
+class Inventario:
+    def __init__(self):
         pass
 
-class Zapatos(IProducto):
-    def seleccionar(self):
-        print("\n")
-        zapatos_productos = ['Adidas', 'Nike', 'New Balance']
-        zapatos_precios = [299, 249, 199]
-        zapatos_descripcion = ['Talla 9', 'Talla 8', 'Talla 7']
-        numero_producto = 1
-        for i in range(len(zapatos_precios)):
-            print(f"{numero_producto}. {zapatos_productos[i]} tiene un precio de ${zapatos_precios[i]}.000: {zapatos_descripcion[i]}")
-            numero_producto += 1
-        print("\n")
-        try:
-            eleccion_carrito = int(input("¿Deseas agregar algun producto al carrito de compras? \n"
-                                + "1. Si \n"
-                                + "2. Volver al inicio \n"
-                                + "3. Salir de la tienda \n"))
-        except ValueError:
-            print("\nLo siento, no te entendí\n"
-                  + "Volviendo al inicio...\n")
-            time.sleep(2)
-            main()
-        else:
-            if eleccion_carrito == 1:
-                print("\n")
-                try:
-                    eleccion_producto = int(input("Indique el numero del producto que desea agregar: \n"))
-                except ValueError:
-                    print("\nLo siento, no te entendí\n"
-                          + "Volviendo al inicio...\n")
-                    time.sleep(2)
-                    main()
-                else:
-                    if eleccion_producto <= 0 or eleccion_producto > len(zapatos_precios):
-                        print("\nError...")
-                        print("Volviendo al inicio...\n")
-                        time.sleep(2)
-                        main()
-                    else:
-                        try:
-                            cantidad_productos = int(input("Escriba la cantidad deseada del producto agregar a su compra: \n"))
-                        except ValueError:
-                            print("\nLo siento, no te entendí\n"
-                                  + "Volviendo al inicio...\n")
-                            time.sleep(2)
-                            main()
-                        else:
-                            if cantidad_productos > 0:
-                                while i <= cantidad_productos:
-                                    Carrito_compras().agregar_producto(zapatos_productos[eleccion_producto-1], zapatos_precios[eleccion_producto-1], zapatos_descripcion[eleccion_producto-1])
-                                    i+=1
-                                print("Producto/s agregado/s\n"
-                                    + "Volviendo al inicio...\n")
-                                time.sleep(2)
-                                main()
-                            else:
-                                print("\nLo siento, no se pueden agregar 0 productos\n"
-                                      + "Volviendo al inicio...\n")
-                                time.sleep(2)
-                                main()
-            elif eleccion_carrito == 2:
-                print("Volviendo al inicio...\n")
-                time.sleep(2)
-                main()
-            elif eleccion_carrito == 3:
-                exit("Gracias por visitar nuestra tienda.")
-            else:
-                print("\nLo siento, no te entendí\n"
-                      + "Volviendo al inicio...\n")
-                time.sleep(2)
-                main()
- 
-class Camisetas(IProducto):
-    def seleccionar(self):
-        camisetas_productos = []
-        camisetas_precios = []
-        camisetas_descripcion = []
-        pass
-
-class Pantalones(IProducto):
-    def seleccionar(self):
-        pantalones_productos = []
-        pantalones_precios = []
-        pantalones_descripcion = []
+    def mostrar_info(self):
         pass
     
-class Sacos(IProducto):
-    def seleccionar(self):
-        sacos_productos = []
-        sacos_precios = []
-        sacos_descripcion = []
-        pass
+    """
+    def mostrar_productos(self):
+        for i,x in enumerate(self.pokemons):
+            print(f'{i+1}) {pk.Pokemon(x()).ver_nombre()}')
+    """
 
-class Celulares(IProducto):
-    def seleccionar(self):
-        celulares_productos = []
-        celulares_precios = []
-        celulares_descripcion = []
-        pass
+class Zapato_Inventario(Inventario):
+    def __init__(self, zapato:Zapatos):
+        self.zapato = zapato
 
-class Televisores(IProducto):
-    def seleccionar(self):
-        televisores_productos = []
-        televisores_precios = []
-        televisores_descripcion = []
-        pass
+    def mostrar_info(self):
+        print(f'Producto: {self.zapato.producto}\n'
+              f'Precio:   ${self.zapato.precio}\n'
+              f'Talla:    {self.zapato.talla}')
+        
+    def precio(self):
+        return self.zapato.precio
 
-class Audifonos(IProducto):
-    def seleccionar(self):
-        audifonos_productos = []
-        audifonos_precios = []
-        audifonos_descripcion = []
-        pass
+class Celular_Inventario(Inventario):
+    def __init__(self, celular:Celulares):
+        self.celular = celular
+
+    def mostrar_info(self):
+        print(f'Producto:          {self.celular.producto}\n'
+              f'Precio:            ${self.celular.precio}\n'
+              f'Almacenamiento:    {self.celular.almacenamiento}\n'
+              f'Procesador:        {self.celular.procesador}\n'
+              f'Sistema Operativo: {self.celular.sistema_operativo}\n'
+              f'Memoria RAM:       {self.celular.memoria_ram}')
+        
+    def precio(self):
+        return self.celular.precio
     
-class Carrito_compras:
+#------------------------------------------------------------LISTAS--------------------------------------------------------------------------
+class ListaZapatos():
+    def __init__(self):
+        self.zapatos = [Adidas, NewBalance, Nike]
+
+    def ver_lista_zapatos(self):
+        for i, x in enumerate(self.zapatos):
+            print(f'{i+1} {Zapato_Inventario(x()).mostrar_info()}\n')
+
+#------------------------------------------------------------CARRITO--------------------------------------------------------------------------
+class Carrito_cliente:
     def __init__(self):
         self.productos = []
-        self.precios = []
-        self.descripcion = []
 
-    def agregar_producto(self, producto, valor, descripcion):
-        self.productos.append(producto)
-        self.precios.append(valor)
-        self.descripcion.append(descripcion)
+    def agregar_producto(self, producto:Inventario, cantidad):
+        for i in range(cantidad):
+            self.productos.append(producto)
 
-    def ver_carrito(self):
-        if len(self.descripcion) <= 0:
-            print("\nSu carrito esta vacio.\n"
-                  +"Volviendo al inicio...\n")
+    def mostrar_carrito(self):
+        if len(self.productos) <= 0:
+            print('\nCarrito de compras vacio\n'
+                  'Te invitamos a revisar las diferentes categorias que tiene nuestra tienda\n'
+                  'Volviendo al inicio...\n')
             time.sleep(2)
             main()
         else:
-            print("\nLos productos en su carrito son los siguientes:")
-            for i in range(len(self.descripcion)):
-                print(f"{self.productos[i]} ${self.precios[i]}.000: {self.descripcion[i]}")
-            print(f"\nEl precio total de los productos en el carrito es de ${self.calcular_precio()}.000")
-    
-    def calcular_precio(self):
-        total = sum(self.precios)
-        return total
-    
-    def terminar_compra(self):
-        if len(self.precios) <= 0:
-            pass
+            precio_total = 0
+            for i in self.productos:
+                i.mostrar_info()
+                precio_total += i.precio()
+                print('\n')
+            print(f'El precio total del carrito de compras es de ${precio_total}')
 
 def main():
+    cliente = Carrito_cliente()
     try:
-        eleccion_main = int(input("Estos son las categorias que tenemos disponibles: \n"
-                                    + "1. Pantalones \n"
-                                    + "2. Camisetas \n"
-                                    + "3. Zapatos \n"
-                                    + "4. Sacos \n"
-                                    + "5. Celulares \n"
-                                    + "6. Televisores \n"
-                                    + "7. Audifonos \n"
-                                    + "8. Carrito de compras \n"
-                                    + "9. Finalizar compra \n"
-                                    + "0. Salir de la tienda \n"
-                                    + "¿Cual categoria deseas revisar? \n"))
+        eleccion_cliente = int(input('Estas son las categorias disponibles:\n'
+                                     '1. Zapatos\n'
+                                     '2. Camisetas\n'
+                                     '3. Celulares\n'
+                                     '4. Televisores\n'
+                                     '5. Ver carrito de compras\n'
+                                     '6. Salir de la tienda\n'
+                                     '¿Cual categoria deseas revisar?\n'))
     except ValueError:
-        print("\n Lo siento, no te entendí \n"
-              + "Volviendo al inicio...\n")
-        time.sleep(2)
+        print('\nERROR\n'
+              'Volviendo al inicio...\n')
+        time.sleep(1)
         main()
     else:
-        if eleccion_main == 1:
-            Pantalones().seleccionar()
-        elif eleccion_main == 2:
-            Camisetas().seleccionar()
-        elif eleccion_main == 3:
-            Zapatos().seleccionar()
-        elif eleccion_main == 4:
-            Sacos().seleccionar()
-        elif eleccion_main == 5:
-            Celulares().seleccionar()
-        elif eleccion_main == 6:
-            Televisores().seleccionar()
-        elif eleccion_main == 7:
-            Audifonos().seleccionar()
-        elif eleccion_main == 8:
-            Carrito_compras().ver_carrito()
-        elif eleccion_main == 9:
-            Carrito_compras().terminar_compra()
-        elif eleccion_main == 0:
-            exit("Gracias por visitar nuestra tienda.")
-        else:
-            print("\nLo siento, no te entendí\n"
-                  + "Volviendo al inicio...\n")
-            time.sleep(2)
-            main()
+        if eleccion_cliente == 1:
+            ListaZapatos().ver_lista_zapatos()
+        elif eleccion_cliente == 2:
+            pass
+        elif eleccion_cliente == 3:
+            pass
+        elif eleccion_cliente == 4:
+            pass
+        elif eleccion_cliente == 5:
+            cliente.mostrar_carrito()
+        elif eleccion_cliente == 6:
+            time.sleep(1)
+            exit('\nGracias por visitar nuestra tienda.\n')
 
 if __name__ == "__main__":
     main()
